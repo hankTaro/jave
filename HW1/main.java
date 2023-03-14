d769227040962d792edbff893b668aecef226be4
hankTaro <107128309+hankTaro@users.noreply.github.com>
2023-03-15 01:27:24 +0800

    Update main.java

--- HW1/main.java
+++ HW1/main.java
@@ -1,10 +1,38 @@
-
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main
 {
 	public static void main(String[] args) {
-		ArrayList<int> score = new ArrayList<int>();
+	    Scanner keyboard = new Scanner(System.in);
+	    
+		ArrayList<Integer> score = new ArrayList<Integer>();
+		while(keyboard.hasNextInt())
+        {
+            score.add(keyboard.nextInt());
+        }
+        
 		
+		int n = score.size();
+		int sum = 0;
+		int ptr = 0;
+		
+		for(int i = 0; i < 10 ; i++)
+		{
+		    if(score.get(ptr) == 10){
+		        sum += score.get(ptr) + score.get(ptr + 1) + score.get(ptr + 2);
+		        ptr++;
+		    }
+		    else if(score.get(ptr) + score.get(ptr + 1) == 10){
+		        sum += score.get(ptr) + score.get(ptr + 1) + score.get(ptr + 2);
+		        ptr += 2;
+		    }
+		    else{
+		        sum += score.get(ptr) + score.get(ptr + 1);
+		        ptr += 2;
+		    }
+		}
+		
+		System.out.println(sum);
 	}
 }
